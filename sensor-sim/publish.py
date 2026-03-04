67d9fd8940c78dadc0474fb9e07e87735a33d050
Michiel Geeraert <michiel.geeraert@outlook.com>
2026-03-04 12:39:04 +0100

    changes publish.py

--- sensor-sim/publish.py
+++ sensor-sim/publish.py
@@ -6,20 +6,20 @@ port = int(os.getenv("MQTT_PORT", "1883"))
 user = os.getenv("MQTT_USER", "")
 pw   = os.getenv("MQTT_PASS", "")
 
-def on_connect(client, userdata, flags, rc, properties=None):
-    print("Connected:", rc)
 
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-client.on_connect = on_connect
-if user:
-    client.username_pw_set(user, pw)
+def on_connect(client, userdata, flags, rc, properties=None): # Callback wordt opgeroepen als client met broker verbindt
+    print("Connected:", rc)# rc = result code (0 = OK)
 
-client.connect(host, port, 60)
+# Maak een MQTT client aan
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)# CallbackAPIVersion.VERSION2 is nodig voor nieuwe paho-mqtt versies
+client.on_connect = on_connect # Koppel de on_connect callback aan de client
+
+client.connect(host, port, 60) # 60 = keepalive
 client.loop_start()
 
 while True:
-    joystick = {"x": random.randint(-50, 1100), "y": random.randint(0, 1023)}  # soms fout
-    buttons = {"a": random.choice([0, 1, 2]), "b": random.choice([0, 1])}      # soms fout
+    joystick = {"x": random.randint(0, 1023), "y": random.randint(0, 1023)}  
+    buttons = {"a": random.choice([0, 1, 2]), "b": random.choice([0, 1, 2])}     
     client.publish("controller/joystick", json.dumps(joystick), qos=0, retain=False)
     client.publish("controller/button", json.dumps(buttons), qos=0, retain=False)
     time.sleep(1)
