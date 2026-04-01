7aa5e4f53f52677c86a59fafcf6d4c2734f28485
Michiel Geeraert <michiel.geeraert@outlook.com>
2026-04-01 11:29:01 +0200

    cleanup docker node-red

--- nodered/lib/secretKey.js
+++
@@ -1,5 +0,0 @@
-// Fixed credential encryption key
-// Use this in settings.js: credentialSecret: require('./lib/secretKey').key
-module.exports = {
-    key: "mF0B9oNC8xw5l1o_yw3asTWLtIhmGmk2WdJHioLG627A1IqmsgXnZuc7GmNgzzMMplAry7O8EzUFVL9EdKyalQ=="
-};
